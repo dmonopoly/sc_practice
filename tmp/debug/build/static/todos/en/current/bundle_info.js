62f28cf8fc63c00d885a5b21b26e5662a3b2ce22
David Zhang <divadz3@yahoo.com>
2011-01-27 10:01:53 -0500

    debug cache changes

--- tmp/debug/build/static/todos/en/current/bundle_info.js
+++ tmp/debug/build/static/todos/en/current/bundle_info.js
@@ -5,7 +5,7 @@
 
           SC.BUNDLE_INFO[target_name] = {
             requires: ['sproutcore/empty_theme','sproutcore/debug','sproutcore/testing'],
-            styles:   ['/static/sproutcore/standard_theme/en/current/stylesheet.css?1295326496'],
+            styles:   ['/static/sproutcore/standard_theme/en/current/stylesheet.css?1293936763'],
             scripts:  []
           }
         })();
